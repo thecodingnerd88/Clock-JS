1d96e2868f1b11698484c64554bc4bc737702f11
Thecodingnerd88 <40854335+thecodingnerd88@users.noreply.github.com>
2018-10-07 06:12:34 +0300

    Add files via upload

---
+++ Clock.js
@@ -0,0 +1,18 @@
+var secondHand = document.querySelector(".second-hand");
+var minuteHand = document.querySelector(".minute-hand");
+var hourHand = document.querySelector(".hour-hand");
+
+function setDate(){
+ var now = new Date();
+ var seconds = now.getSeconds();
+ var secondsDegrees = ((seconds/60)*360) +90;
+ secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+var minutes=now.getMinutes();
+var minutesDegrees = ((minutes/60)*360)+90;
+minuteHand.style.transform= `rotate(${minutesDegrees}deg)`;
+var hours=now.getHours();
+var hoursDegrees = ((hours/12)*360)+90;
+hourHand.style.transform= `rotate(${hoursDegrees}deg)`;
+console.log(seconds,minutes);
+};
+setInterval(setDate,1000);
